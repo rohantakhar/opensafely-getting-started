84b35a8d78fc06393b3cf8d39ebdf365b104252f
Rohan Takhar <rohantakhar@users.noreply.github.com>
2026-03-25 15:23:37 +0000

    Generate age histograms

--- analysis/dataset_definition.py
+++ analysis/dataset_definition.py
@@ -12,3 +12,18 @@ has_registration = practice_registrations.for_patient_on(
 dataset.define_population(has_registration)
 
 dataset.sex = patients.sex
+from ehrql import create_dataset
+from ehrql.tables.tpp import patients, practice_registrations
+
+dataset = create_dataset()
+
+index_date = "2020-03-31"
+
+has_registration = practice_registrations.for_patient_on(
+    index_date
+).exists_for_patient()
+
+dataset.define_population(has_registration)
+
+dataset.sex = patients.sex
+dataset.age = patients.age_on(index_date)
